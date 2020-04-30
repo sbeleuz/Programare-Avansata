0cdde69caf91d0bc69beeebb720bebe20319bc00
StefanBeleuz <58983731+StefanBeleuz@users.noreply.github.com>
2020-04-30 13:11:38 +0300

    Add files via upload

--- lab10/ServerApplication/ClientThread.java
+++ lab10/ServerApplication/ClientThread.java
@@ -1,4 +1,42 @@
+//package app;
+//
+//import game.Game;
+//import game.Player;
+//
+//import java.net.Socket;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class ClientThread extends Thread {
+//    private final Socket socket;
+//    public static volatile List<Game> activeGames = new ArrayList<>();
+//
+//    public ClientThread(Socket socket) {
+//        this.socket = socket;
+//    }
+//
+//    public void run() {
+//        activeGames.removeIf(game -> game.players == 0); // cleanup games with no players
+//
+//        boolean gameFound = false;
+//        for (Game game : activeGames) {
+//            if (game.players == 1) { // found a game with a waiting player, join
+//                gameFound = true;
+//                game.players++;
+//                new Player(socket, 2, game).start();
+//                break;
+//            }
+//        }
+//        if (!gameFound) { // no waiting players, create a new game
+//            Game game = new Game();
+//            activeGames.add(game);
+//            new Player(socket, 1, game).start();
+//        }
+//    }
+//}
 package app;
+
+import com.jcraft.jsch.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,21 +53,23 @@ public class ClientThread extends Thread {
 
     public void run() {
         try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             boolean running = true;
             while (running) {
                 // get the request from client
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 String request = in.readLine();
+                String response = "Server received the request: " + request + ".";
                 if (request.equals("exit")) {
+                    running = false;
+                } else if (request.equals("stop")) {
+                    GameServer.RUNNING = false;
+                    response = "Server stopped!";
                     running = false;
                 }
 
                 // send the response to client
-                PrintWriter out = new PrintWriter(socket.getOutputStream());
-                String response = "Server received the request: " + request + ".";
-
                 out.println(response);
-                out.flush();
             }
         } catch (IOException e) {
             System.err.println(e.getMessage());
--- lab10/ServerApplication/GameServer.java
+++ lab10/ServerApplication/GameServer.java
@@ -1,3 +1,22 @@
+//package app;
+//
+//import java.io.IOException;
+//import java.net.ServerSocket;
+//
+//public class GameServer {
+//    public static final int PORT = 2020;
+//
+//    public GameServer() {
+//        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+//            System.out.println("Game server is running...");
+//            while (true) {
+//                new ClientThread(serverSocket.accept()).start();
+//            }
+//        } catch (IOException e) {
+//            System.err.println(e.getMessage());
+//        }
+//    }
+//}
 package app;
 
 import java.io.IOException;
@@ -6,17 +25,24 @@ import java.net.Socket;
 
 public class GameServer {
     public static final int PORT = 2020;
+    public static volatile boolean RUNNING = true; // thread-safe
 
     public GameServer() {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-            while (true) {
+            serverSocket.setSoTimeout(10 * 1000); // timeout for accept()
+            while (RUNNING) {
                 System.out.println("Waiting for a client ...");
-                Socket socket = serverSocket.accept();
-
-                new ClientThread(socket).start();
+                try {
+                    Socket socket = serverSocket.accept();
+                    new ClientThread(socket).start();
+                } catch (java.io.InterruptedIOException e) {
+                    System.out.println(e.getMessage());
+                }
             }
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
+
+        System.out.println("Server stopped!");
     }
 }
